d3ff8f3ff3ee43fa8d68c3b2fb3e5d7e9958e92e
Joel Edwards <joeledwards@gmail.com>
2014-08-22 23:24:39 -0600

    The aescrypt.py script now successfully identifies and reads (doesn't decrypt) all parts of the AES file. Updated pack.py to use hashlib instead of sha256 for now.

--- aescrypt.py
+++ aescrypt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import binascii
 import os
 import struct
 import time
@@ -9,17 +10,26 @@ class InvalidFileFormatException(Exception):
 
 class AESCrypt(object):
     def __init__(self, password):
-        self.version = None
-        self.size = None
-        self.password = password
-        self.IV_key = None
-        self.IV = None
-        self.key = None
-        self.HMAC = None
-        self.extensions = None
         self.aes_fh = None
+        self.password = password
+
+        self.version = None
+        self.extensions = None
+        self.key_iv = None
+        self.iv = None
+        self.key = None
+        self.key_hmac = None
+        self.hmac = None
+
+        self.padded_size = None
+        self.original_size = None
+
+    def get_password_key(self):
+        # todo convert password into a key
+        pass
 
     def encrypt(self, raw_file, aes_file):
+        pass
 
     def decrypt(self, aes_file, raw_file):
         if not os.path.exists(aes_file):
@@ -28,26 +38,34 @@ class AESCrypt(object):
         if not os.path.isfile(aes_file):
             raise InvalidFileFormatException("Path exists, but is not a regular file:" + aes_file)
 
-        aes_size = os.path.getsize(aes_file)
+        self.aes_size = os.path.getsize(aes_file)
 
-        if aes_size < 53:
+        if self.aes_size < 53:
             raise InvalidFileFormatException("File format is invalid or file is incomplete")
 
         self.aes_fh = open(aes_file, 'r')
 
         self.read_header()
 
-        if self.version > 0:
+        if self.version == 0:
+            self.get_size_modulus()
+        else:
             self.read_reserved()
 
         if self.version == 2:
             self.read_extensions()
 
-        if self.version > 0:
-            self.read_key_and_iv()
+        if self.version == 0:
+            self.get_iv()
+            self.get_length()
+            self.get_hmac()
         else:
-            self.make_key_and_iv()
-
+            self.get_key_iv()
+            self.get_iv_and_key()
+            self.get_key_hmac()
+            self.get_length()
+            self.get_size_modulus()
+            self.get_hmac()
 
     def read_header(self):
         header = self.aes_fh.read(4)
@@ -55,39 +73,41 @@ class AESCrypt(object):
 
         if aes_id != "AES":
             raise InvalidFileFormatException("File is of the wrong type or is corrupt")
-        if version == 0 and aes_size < 53:
-            raise InvalidFileFormatException("File is marked as verison " + version + " but is too small")
-        if version > 0 and version < 3 and aes_size < 134:
-            raise InvalidFileFormatException("File is marked as verison " + version + " but is too small")
-        raise InvalidFileFormatException("Version (" + version + ") is not supported")
+        if self.version < 0 or self.version > 2:
+            raise InvalidFileFormatException("Version (" + str(self.version) + ") is not supported")
+        if self.version == 0 and self.aes_size < 53:
+            raise InvalidFileFormatException("File is marked as verison " + str(self.version) + " but is too small")
+        if self.version > 0 and self.version < 3 and self.aes_size < 134:
+            raise InvalidFileFormatException("File is marked as verison " + str(self.version) + " but is too small")
 
-    def read_reserved(self);
+    def read_reserved(self):
         self.aes_fh.read(1) # skip reserved octet
         
     def read_extensions(self):
         self.extensions = {}
-        ext_index = -1 
+        extension_index = -1 
         more = True
         while more:
-            ext_index += 1
-            extension = self.read_extensions()
+            extension_index += 1
+            extension = self.read_extension()
             if extension is None:
                 more = False
             else:
-                self.extensions[ext_index] = extension
+                self.extensions[extension_index] = extension
 
     def read_extension(self):
-        ext_len = struct.unpack("!H", self.aes_fh.read(2))
-        if ext_len == 0:
+        extension_len = struct.unpack("!H", self.aes_fh.read(2))[0]
+        if extension_len == 0:
             return None
-        extension = self.aes_fh.read(ext_len)
+        extension = self.aes_fh.read(extension_len)
         return self.parse_extension(extension)
 
     def parse_extension(self, extension):
         z_str = struct.pack("!B", 0)
 
-        id_end = ext_len
-        for i in range(0, ext_len):
+        extension_len = len(extension)
+        id_end = extension_len
+        for i in range(0, extension_len):
             if extension[i] == z_str:
                 id_end = i
                 break
@@ -95,16 +115,85 @@ class AESCrypt(object):
         if id_end == 0:
             return ("", "") # an empty extensions
 
-        ext_id = struct.unpack("!" + id_end + "s", extension[:id_end])
+        extension_id = struct.unpack("!" + str(id_end) + "s", extension[:id_end])[0]
         data_start = id_end + 1
+        data_len = extension_len - data_start
 
-        ext_data = ""
-        if data_start < ext_len:
-            ext_data = struct.unpack("!" + data_start + "s", extension[data_start:])
+        extension_data = ""
+        if data_start < extension_len:
+            extension_data = struct.unpack("!" + str(data_len) + "s", extension[data_start:])[0]
 
-        return (ext_id, ext_data)
+        return (extension_id, extension_data)
 
+    def get_key_iv(self):
+        self.key_iv = self.aes_fh.read(16)
 
+    def get_iv(self):
+        self.iv = self.aes.fh.read(16)
+
+    def get_iv_and_key(self):
+        iv_key = self.aes_fh.read(48) # TODO: remove
+        self.iv = iv_key [:16] 
+        self.key = iv_key[16:]
+        return  # TODO: remove
+
+        password_key = self.get_password_key()
+
+        if self.version == 0:
+            self.iv =  self.aes_fh.read(16)
+            self.key = password_key
+            return 
+
+        # read cyphertext containing real key and IV from the file
+        key_iv_cypher = self.aes_fh.read(48)
+
+        # decrypt the real key and IV
+        iv_key = ""#decrypt
+        self.iv = iv_key[:16]
+        self.key = iv_key[16:]
+
+    def get_key_hmac(self):
+        self.key_hmac = self.aes_fh.read(32)
+
+    def get_length(self):
+        start_offset = self.aes_fh.tell()
+        print "cipher text start offset is", start_offset
+        self.aes_fh.seek(-32 if self.version == 0 else -33, 2)
+        end_offset = self.aes_fh.tell()
+        print "cipher text end offset is", end_offset
+        self.padded_size = end_offset - start_offset
+
+    def get_size_modulus(self):
+        print "file size is", self.aes_size
+        print "offset before size_modulus read is", self.aes_fh.tell()
+        self.size_modulus = struct.unpack("!B", self.aes_fh.read(1))[0]
+        self.original_size = self.padded_size - (16 - self.size_modulus)
+
+    def get_hmac(self):
+        self.hmac = self.aes_fh.read(32)
+
+def hexlify(value):
+    return binascii.hexlify(value) if value is not None else value
+
+def main():
+    aescrypt = AESCrypt("buzuli")
+    aescrypt.decrypt("test.txt.aes", "test.txt.out")
+
+    print "        password:", aescrypt.password
+    print "         version:", aescrypt.version
+    print "      extensions:", aescrypt.extensions
+    print "          key iv:", hexlify(aescrypt.key_iv)
+    print "              iv:", hexlify(aescrypt.iv)
+    print "             key:", hexlify(aescrypt.key)
+    print "        key hmac:", hexlify(aescrypt.key_hmac)
+    print "    size modulus:", aescrypt.size_modulus
+    print "            hmac:", hexlify(aescrypt.hmac)
+    print "   aes file size:", aescrypt.aes_size
+    print "     padded size:", aescrypt.padded_size
+    print "   original size:", aescrypt.original_size
+
+if __name__ == "__main__":
+    main()
 
         
 
--- pack.py
+++ pack.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-
+import binascii
 import struct
+import hashlib
 
 packed = struct.pack("!4s", "Joel")
-result = struct.unpack("!3s", packed[:3])
+result = struct.unpack("!3s", packed[:3])[0]
 print result
 
 packed = struct.pack("!4sB", "Joel", 0)
 print type(packed[4])
 print packed[4] == 0
 print packed[4] == struct.pack("!B", 0)
+
+import sha256
+message = "Joel"
+packed = struct.pack("!4sB", message, 0)
+print binascii.hexlify(hashlib.sha256(packed).digest())
